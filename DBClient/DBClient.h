79743c3d076beda0933f77c7ae69dde52cdf642c
Anton Vodolazkyi <vodolazkyi@MacBook-Pro-Anton.local>
2017-05-29 13:34:16 +0300

    Add Carthage

---
+++ DBClient/DBClient.h
@@ -0,0 +1,19 @@
+//
+//  DBClient.h
+//  DBClient
+//
+//  Created by Anton Vodolazkyi on 29.05.17.
+//  Copyright © 2017 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DBClient.
+FOUNDATION_EXPORT double DBClientVersionNumber;
+
+//! Project version string for DBClient.
+FOUNDATION_EXPORT const unsigned char DBClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DBClient/PublicHeader.h>
+
+
